34b786293250e7d4fba5cdba7d42f06f387329ec
Talgat Shashakhmetov <talgatks@gmail.com>
2024-12-17 19:07:55 -0800

    Release v1.2.3.1

--- src/upload/admin/controller/extension/module/extended_layout.php
+++ src/upload/admin/controller/extension/module/extended_layout.php
@@ -7,7 +7,7 @@ class ControllerExtensionModuleExtendedLayout extends Controller {
 
 	public 	$_route 	= 'extension/module/extended_layout';
 	public 	$_model 	= 'model_extension_module_extended_layout';
-	private $_version 	= '1.2.3';
+	private $_version 	= '1.2.3.1';
 
 	public function install() {
 		$this->load->model($this->_route);
